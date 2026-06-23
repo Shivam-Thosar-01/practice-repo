8f8e834a5e839544dc1eaae99d87c0a24c70453d
Shivam-Thosar-01 <shivamthosar099@gmail.com>
2026-06-23 19:34:40 +0530

    today i learn abot a variable and datatype in js

---
+++ lecture40.js
@@ -0,0 +1,40 @@
+// today i learn about a js it is a scripting language
+// it can be used at both side client side or serverside 
+// ECMA is a stander used for write a js (set of rule)
+// to parse a javascript ,js engine is required
+
+// to print any value "console.log("hii")"
+// we can runn js in 
+// 1.browser
+// 2.vs code 
+//     in vs we ca use js by using scripting tag this can be placed at body of html,at start of body tag,at end of body tag can also used at head tag
+//  i also learn variable and datatype
+ 
+//  1.variable 
+//     it is a named memory location 
+
+//     a)var ->it is global scoped, function scoped not an block scopedit can be redefined,redeclare and can be manuplate
+//     b)let ->it is a block scoped it cannot be redefined,redeclare 
+//     c)const -> it is used to define constant value ,once value declare then it cannot be changed,cannot be redefined,redeclare and can be manuplate
+
+
+//    #variab;e naming conventin
+//    1.variabe can start with _,&,Number
+//    2.it cannot use predefine keywords
+//    3.it does not contain VideoColorSpace
+//    4.variable name must use camel case
+//    5.vaariable name must be meaningful 
+
+// 2.datatype
+//     data type can be used for define type of data and size of datathere are two type of data type
+//     a)premitive datatype
+//     b)non-premitive datatype
+
+
+
+// let a=10;
+// let a=55;
+// var a=60;
+// var b=30;
+const b=44;
+console.log(b);
